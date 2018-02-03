939e452e59619ca316ee04e09741491d568fd864
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    power: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- prop.mk
+++ prop.mk
@@ -144,7 +144,6 @@ ro.vendor.qti.sys.fw.bservice_enable=true
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.extension_library=libqti-perfd-client.so \
 ro.vendor.perf.scroll_opt=true
 
 # Netmgrd
