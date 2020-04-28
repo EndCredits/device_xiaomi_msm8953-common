020ad7730b3c9ad40c3bef268509b0b3da28bff9
Jun Wang <junwan@codeaurora.org>
2020-04-29 00:44:19 +0800

    msm8953-common: perf: Enable the pre-rendering feature
     * Pre-rendering feature can improve FPS and reduce the janks
       in scrolling cases.
    
    Change-Id: I2152d0a1a8f9e337cb0355c8874e72f0f3c2cb46
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- prop.mk
+++ prop.mk
@@ -144,7 +144,8 @@ ro.vendor.qti.sys.fw.bservice_enable=true
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.extension_library=libqti-perfd-client.so
+ro.vendor.extension_library=libqti-perfd-client.so \
+ro.vendor.perf.scroll_opt=true
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
