b95c66a822079d4d760ea049ed56bfa8c042136c
Tim Lin <timtmlin@google.com>
2020-09-26 10:28:54 +0800

    msm8953-common: qcril: fix MT SMS sent during power off
    qcril will block any further MT SMS if previous one is not ACKed.
    For this bug, IMS service is not ready and resulted in qcril stuck in blocking state.
    This solution will add 30s timer to reset blocking state if there is no ACK from Telephony.
    
    Bug: 169549852
    Test: MT SMS during power off
    Change-Id: I6f52170654925143aeb55f8bc0cf4c379717e718
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- prop.mk
+++ prop.mk
@@ -167,6 +167,7 @@ persist.vendor.radio.apm_sim_not_pwdn=1 \
 persist.radio.multisim.config=dsds \
 persist.sys.fflag.override.settings_provider_model=false \
 persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.mt_sms_ack=30 \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
 persist.vendor.data.iwlan.enable=true \
