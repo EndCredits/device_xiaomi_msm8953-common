daa1ca5000ac0dc3927aab8cf1815965a489fb21
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    msm8953-common: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -151,9 +151,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.0-impl:64 \
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.4-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
