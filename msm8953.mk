4f0236c0850d3417498f078c2348447040e43120
EndCredits <alicization.han@gmail.com>
2021-10-01 16:46:43 +0800

    Revert "msm8953-common: Down soundtrigger hal to 2.1"
    This reverts commit 893eb1a84e157a5f3ba3f9cf3a62b3063dfdc91f.

--- msm8953.mk
+++ msm8953.mk
@@ -80,7 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
