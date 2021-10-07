a508b559e547975ba274c177ad41bc88358393fe
daniml3 <danimoral1001@gmail.com>
2021-10-07 16:23:30 +0200

    msm8953-common: build the HIDL interfaces that were removed from VNDK
    * Commit 37aeef03d43b (build/make) removes most HIDL interfaces from
      the VNDK, and those libraries are still needed
    
    * Take a list of the removed libraries and build them to vendor
    
    Change-Id: I83478c8dda88fc244357ad777ddf4df9b5a5218b
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -409,7 +409,6 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
 
 $(foreach target, $(shell cat $(LOCAL_PATH)/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
 
-# Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-V1-ndk_platform.vendor \
     android.hardware.vibrator@1.3.vendor \
