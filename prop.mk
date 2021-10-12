d383b75e30be1a04a03b15c5e06956c754f8c95c
neonmicky <neonmicky30@gmail.com>
2021-10-12 18:13:55 +0000

    msm8953-common: Disable Blur by default
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- prop.mk
+++ prop.mk
@@ -40,6 +40,10 @@ qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
 ro.qualcomm.bt.hci_transport=smd
 
+# Blur
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.sf.disable_blurs=1
+
 # Bpf
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.kernel.ebpf.supported=1
