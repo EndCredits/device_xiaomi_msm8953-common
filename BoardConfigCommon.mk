00c1396bb1a1f30aa8ec3abf63282aa96a3126fd
EndCredits <alicization.han@gmail.com>
2021-10-26 21:44:46 +0800

    msm8953-common: Drop qcom hardware variety

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,8 +42,6 @@ BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 PRODUCT_BOARD_PLATFORM := msm8953
 PRODUCT_USES_QCOM_HARDWARE := true
 
-OVERRIDE_QCOM_HARDWARE_VARIANT := msm8996-r
-
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
