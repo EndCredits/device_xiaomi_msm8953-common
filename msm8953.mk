d5677af292c6f7337ea5fd98bee3b9754deb9e17
Crepuscluar <64133324+EndCredits@users.noreply.github.com>
2021-11-01 18:27:27 +0800

    msm8953-common: Stop building extra VNDK Versions

--- msm8953.mk
+++ msm8953.mk
@@ -405,8 +405,6 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
-PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
-
 $(foreach target, $(shell cat $(LOCAL_PATH)/vndk.txt), $(eval PRODUCT_PACKAGES += $(target).vendor))
 
 # Vibrator
