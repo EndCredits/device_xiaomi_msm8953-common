175b447f6435cb368e8d54385238854c0cfe8e4e
EndCredits <alicization.han@gmail.com>
2021-11-05 13:52:25 +0800

    msm8953-common: Deprcate Lineage Hals Building

--- msm8953.mk
+++ msm8953.mk
@@ -250,8 +250,8 @@ PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_msm8953
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -388,8 +388,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.trust@1.0-service
 
 # USB HAL
 PRODUCT_PACKAGES += \
