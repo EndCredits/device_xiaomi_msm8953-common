e0bc54a41d939047ba087057b121ea1721061409
EndCredits <alicization.han@gmail.com>
2021-11-06 15:09:12 +0800

    msm8953-common: manifest: Update HAL target level to 3 and add power hals

--- msm8953.mk
+++ msm8953.mk
@@ -302,6 +302,11 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0.vendor \
+    android.hardware.power@1.1.vendor \
+    android.hardware.power@1.2.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
