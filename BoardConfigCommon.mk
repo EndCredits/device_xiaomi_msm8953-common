8540cd6742be574168367d7d25756a5bd1d6f541
EndCredits <alicization.han@gmail.com>
2021-11-06 11:49:42 +0800

    msm8953-common: Introduce new framework compatibility matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,6 +128,7 @@ TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 # HIDL
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(VENDOR_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(VENDOR_PATH)/framework_compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_msm8953
