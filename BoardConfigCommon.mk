709dc4c9e8ccee07289f292800f433dea89c2f05
EndCredits <alicization.han@gmail.com>
2021-12-04 01:41:26 +0800

    msm8953-common: Power mode path fix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,7 +151,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWERHAL_MODE_EXT := $(VENDOR_PATH)/power/power-mode.cpp
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
