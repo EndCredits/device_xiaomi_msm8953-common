8ae7d6b1aa86e0623318d1b885653c92e4178f66
me-cafebabe <me-cafebabe@users.noreply.github.com>
2021-12-14 10:14:16 +0800

    msm8953-common: Add hardware/google/{interfaces,pixel} to soong namespaces
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- msm8953.mk
+++ msm8953.mk
@@ -390,7 +390,9 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Speed up
 PRODUCT_DEXPREOPT_SPEED_APPS += \
