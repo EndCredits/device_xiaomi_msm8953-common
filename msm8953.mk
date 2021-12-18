dbb01230bf06087da579c573fb5faeea0daecfc5
EndCredits <alicization.han@gmail.com>
2021-12-18 22:44:23 +0800

    Revert "[REPICK]msm8953-common: prepare to move to in-tree HALs"
    This reverts commit 32d14e9959cb7ed77435366d9315845ea4381872.

--- msm8953.mk
+++ msm8953.mk
@@ -375,9 +375,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-QCOM_SOONG_NAMESPACE := \
-    $(LOCAL_PATH)/qcom-caf
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
