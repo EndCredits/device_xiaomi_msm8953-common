09eb51322a1447f65a9728888a8d143a0f0d5d4f
EndCredits <alicization.han@gmail.com>
2021-12-31 19:40:50 +0800

    msm8953-common: Remove blur props

--- prop.mk
+++ prop.mk
@@ -40,13 +40,6 @@ qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
 ro.qualcomm.bt.hci_transport=smd
 
-# Blur
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.supports_background_blur=1 \
-persist.sys.sf.disable_blurs=1 \
-ro.sf.blurs_are_expensive=1 \
-debug.sf.disable_backpressure=1
-
 # Bpf
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.kernel.ebpf.supported=1
