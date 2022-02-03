8df9e7a217bd7a5915fd65779b9607b45cbd8951
EndCredits <alicization.han@gmail.com>
2022-02-03 09:29:47 +0800

    Revert "msm8953-common: Add vendor.display.use_smooth_motion flag on vendor.prop"
    This reverts commit ac887f9d553f409e7ed7c0551e54faafb34b739c.

--- prop.mk
+++ prop.mk
@@ -92,8 +92,7 @@ ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1 \
-vendor.display.use_smooth_motion=1
+vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
