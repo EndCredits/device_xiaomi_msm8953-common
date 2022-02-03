21ebbce0062b69b8e05e935b3624be71be5bcc06
EndCredits <alicization.han@gmail.com>
2022-02-03 09:31:07 +0800

    Revert "power: Switch to libperfmgr for powerhint"
    This reverts commit 86b1cd935ce755e1df75718a4f758fbd0bf66173.

--- msm8953.mk
+++ msm8953.mk
@@ -311,17 +311,18 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0.vendor
+
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.onclite-libperfmgr
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0.vendor \
-    android.hardware.power@1.1.vendor \
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Properties
 -include device/xiaomi/msm8953-common/prop.mk
